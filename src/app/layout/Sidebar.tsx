0ad2eaf60e9beb217b52be19ea65cf77a4b105b7
bijenShrestha980 <s.bijen93@gmail.com>
2023-05-29 17:37:38 +0545

    shadow

--- src/app/layout/Sidebar.tsx
+++ src/app/layout/Sidebar.tsx
@@ -2,7 +2,7 @@ import SidebarLinks from "../components/sidebar/SidebarLinks";
 
 const Sidebar = () => {
   return (
-    <div className="h-[110px] md:h-screen w-full bg-white flex flex-col justify-center items-center p-10">
+    <div className="h-[110px] md:h-screen w-full bg-white flex flex-col justify-center items-center p-10 shadow-sm">
       <div className="h-[110px] md:h-screen w-full md:w-[180px] flex flex-row md:flex-col gap-8 justify-between md:justify-center items-baseline md:items-start">
         <h3 className="font-prima">Salom</h3>
         <SidebarLinks />
