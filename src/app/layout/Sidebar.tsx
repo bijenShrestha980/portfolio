0e2e711209d9fc51ebc57e0d7a9ce4ec4bda16e2
bijenShrestha980 <s.bijen93@gmail.com>
2023-06-02 12:38:47 +0545

    नमस्ते

--- src/app/layout/Sidebar.tsx
+++ src/app/layout/Sidebar.tsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
     <div className="fixed bottom-0 h-[90px] md:h-screen w-full bg-white flex flex-col justify-center items-center p-10 shadow-2xl">
       <div className="h-[110px] md:h-screen w-full md:w-[180px] flex flex-row md:flex-col gap-8 justify-between md:justify-center items-baseline md:items-start">
-        <h3 className="font-prima">Salom</h3>
+        <h3 className="font-prima">नमस्ते</h3>
         <SidebarLinks />
       </div>
     </div>
